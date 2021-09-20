072384f4065f5d76f328bd8c05bd24c92a3640f3
Alecto Irene Perez <perez.cs@pm.me>
2021-09-19 21:42:36 -0600

    Switched to using std::system_error 🐈😌

--- include/expat/process.hpp
+++ include/expat/process.hpp
@@ -1,23 +1,20 @@
 #pragma once
-#include <expat/system_error.hpp>
+#include <system_error>
 #include <unistd.h>
+
 namespace expat {
 
 struct pipe_fd {
-    int read_end;
-    int write_end;
+    int read_end = 0;
+    int write_end = 0;
 };
 pipe_fd open_pipe() {
     int arr[2];
-    if(!pipe(arr)) {
-        return pipe_fd{arr[0], arr[1]};
+    if (!pipe(arr)) {
+        return pipe_fd {arr[0], arr[1]};
     } else {
-        throw_errno("Unable to open pipe");
+        throw std::system_error(
+            errno, std::system_category(), "Unable to open pipe");
     }
 }
-
-
-struct process {
-
-};
-}
+} // namespace expat
