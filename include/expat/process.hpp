d313257b0e93472e1ec036a30775dc4458ce4889
Alecto Irene Perez <perez.cs@pm.me>
2021-09-20 06:36:43 -0600

    Allowed run_process to take a array of file descriptors to override 💅💕

--- include/expat/process.hpp
+++ include/expat/process.hpp
@@ -156,6 +156,60 @@ constexpr fd_desc operator"" _input(unsigned long long fd) {
 constexpr fd_desc operator"" _output(unsigned long long fd) {
     return fd_desc {(int)fd, fd_output};
 }
+template <size_t N>
+std::array<fd_desc, N> run_process(
+    std::string_view pathname,
+    std::array<fd_desc, N> const& fds) {
+    process_fd proc;
+
+    std::array<pipe_fd, N> pipes;
+    for (auto& pipe : pipes) {
+        pipe = open_pipe();
+    }
+
+    if (fork_or_throw()) {
+        // We know we're the parent because pid != 0
+
+        // We need to close the end we're not using
+        std::array<fd_desc, N> result;
+        for (int i = 0; i < N; i++) {
+            if (fds[i].category == fd_input) {
+                close(pipes[i].read_end);
+                result[i] = output_fd(pipes[i].write_end);
+            } else {
+                close(pipes[i].write_end);
+                result[i] = input_fd(pipes[i].read_end);
+            }
+        }
+        // the parent gets the write end and the read ends
+        return result;
+    } else {
+        // The pid returned from fork_or_throw() was 0, so we know we're the
+        // child
+
+        // We need to close the end we're not using
+        for (int i = 0; i < N; i++) {
+            if (fds[i].category == fd_input) {
+                close(pipes[i].write_end);
+                dup2_or_throw(pipes[i].read_end, fds[i]);
+            } else {
+                close(pipes[i].read_end);
+                dup2_or_throw(pipes[i].write_end, fds[i]);
+            }
+        }
+
+        std::string pathname_arg = std::string(pathname);
+        std::array<char*, 2> args = {pathname_arg.data(), nullptr};
+
+        execv(pathname_arg.data(), args.data());
+
+        // This only occurs when execv fails
+        throw std::system_error(
+            errno,
+            std::system_category(),
+            "Unable to run execv");
+    }
+}
 
 std::string_view read_or_throw(int fd, std::span<char> buffer) {
     ssize_t bytes_read = read(fd, buffer.data(), buffer.size());
