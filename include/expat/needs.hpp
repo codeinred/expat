4998ec5aa432da2e92d3c843aa85b5ddbfdbefa6
Alecto Irene Perez <perez.cs@pm.me>
2021-09-21 11:25:09 -0600

    Changed implementation of read_all_output to use conduit coroutines 💖🐈

--- include/expat/needs.hpp
+++ include/expat/needs.hpp
@@ -2,25 +2,54 @@
 #include <conduit/util/stdlib_coroutine.hpp>
 
 #include <conduit/coroutine.hpp>
+#include <conduit/future.hpp>
 
 #include <expat/process.hpp>
 
 #include <string>
 
-#define BOOST_ASIO_HAS_CO_AWAIT
-#include <boost/asio/co_spawn.hpp>
-#include <boost/asio/detached.hpp>
-#include <boost/asio/experimental/coro.hpp>
 #include <boost/asio/posix/stream_descriptor.hpp>
 
 namespace expat {
+namespace co = conduit;
 namespace asio = boost::asio;
 namespace posix = boost::asio::posix;
 
+using boost::system::error_code;
 using fd_stream = posix::stream_descriptor;
 
+
+template <class Stream, class Buffer>
+class async_read_some {
+    Stream& stream;
+    Buffer& buffer;
+    size_t result {};
+
+   public:
+    async_read_some(Stream& stream, Buffer& buffer)
+      : stream(stream)
+      , buffer(buffer) {}
+
+    constexpr bool await_ready() const noexcept { return false; }
+    constexpr void await_suspend(std::coroutine_handle<> handle) {
+        stream.async_read_some(
+            asio::buffer(buffer),
+            [this, handle](error_code const& err, size_t bytes_read) {
+                if (err) {
+                    result = 0;
+                } else {
+                    result = bytes_read;
+                }
+                handle.resume();
+            });
+    }
+    constexpr auto await_resume() const noexcept {
+        return std::string_view(buffer.data(), result);
+    }
+};
+
 auto read_all_output(asio::io_context& context, char** argv)
-    -> asio::awaitable<std::string> {
+    -> co::future<std::string> {
     auto [child_out, child_err] = run_process(
         std::array {1_output, 2_output},
         argv);
@@ -29,20 +58,14 @@ auto read_all_output(asio::io_context& context, char** argv)
 
     std::array<char, 1024> buffer;
     std::string result;
-    while (output.is_open()) {
-        size_t num_bytes = co_await output.async_read_some(
-            asio::buffer(buffer),
-            asio::use_awaitable);
-
+    while (true) {
+        std::string_view text = co_await async_read_some(output, buffer);
 
         // If No bytes are read, we break so we can return the result
-        if (num_bytes == 0)
+        if (text.size() == 0)
             break;
 
-        auto section_read = std::string_view(buffer.data(), num_bytes);
-        fmt::print("Read {}\n", section_read);
-        fmt::print("Output open? {}\n", output.is_open());
-        result += section_read;
+        result += text;
     }
 
     co_return result;
